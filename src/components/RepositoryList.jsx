f45b498d4028fa5dca37ebd6b1fa2e13dd3d20b2
Jhordhan <jhordhan.gviva@gmail.com>
2021-10-27 10:22:04 -0300

    configuracoes finais antes de desenvolver em React

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,15 @@
+export function RepositoryList (){
+    return(
+        <section className="rapository-list">
+            <h1>Lista de Repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>
+                        uniform
+                    </strong>
+                </li>
+            </ul>
+        </section>
+    )
+}
